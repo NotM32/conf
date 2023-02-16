d28d069c61a4e601a5faf717f301222ae5db8ef7
m32 <m32@m32.io>
2023-02-15 20:15:47 -0800

    add deploy util

--- flake.nix
+++ flake.nix
@@ -5,49 +5,63 @@
     nixpkgs.url = "github:nixos/nixpkgs";
     home-manager.url = "github:nix-community/home-manager";
 
+    deploy.url = "github:serokell/deploy-rs";
+
   };
 
 
-  outputs = inputs@{ self, nixpkgs, home-manager, ... }: {
+  outputs = inputs@{ self, nixpkgs, home-manager, deploy, ... }:
+    # Variables in the let scope are used for composing the actual user configurations,
+    # host configuration, hardware configurations, deploy nodes etc.
 
-    hosts =
-      let
-        util = import ./util inputs;
+    # They are meant to be the inputs provided to the utility functions that create the
+    # actual flake outputs.
 
-        # Users / Home Conf
-        users = {
-          m32 = import ./config/home.nix;
-        };
+    # While the use of multiple let statements isn't as syntactically satisfying as the use
+    # of an attr set with a complex main function, keeping things scoped like this makes
+    # things a whole lot clearer in terms of what is actually happening.
+    let
+      # - IMPORTS -
+      util = import ./util { inherit inputs; };
 
-      in
-        # Hosts
-        {
-          phoenix = util.makeSystemConfiguration
-            { hardwareConfig = ./machines/phoenix.nix;
-              systemConfig = ./system;
+      # - BEGIN CONFIG -
+
+      # Users / Home Conf
+      users.m32 = import ./config/home.nix;
+
+    in
+    let
+      inherit users util;
+
+      # Host Composition
+      hosts =
+        { # ** Hosts
+          # Desktop
+          phoenix =
+            { hardwareConfig = [ ./machines/phoenix ];
+              systemConfig = [ ./system ];
+              hmUsers = users;
+            };
+
+          # T430 laptop
+          momentum =
+            { hardwareConfig = [ ./machines/phoenix ];
+              systemConfig = [ ./system ];
               hmUsers = users;
             };
         };
+    in
+      # - END CONFIG -
+      {
+        # Configuration is done at this point, and this is the logic that doesn't change often
+        # used to build the actual flake outputs
 
-    nixosConfigurations = {
+        # * NixOS Configurations
+        nixosConfigurations = util.makeSystemConfigurations hosts;
 
-      # phoenix desktop
-      phoenix = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
+        # * deploy-rs outputs
+        deploy.nodes = util.makeDeployNodes hosts;
 
-        modules = [
-          ./machines/phoenix.nix
-          home-manager.nixosModules.home-manager {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.m32 = import ./config/home.nix;
-          }
-        ];
-
-        specialArgs = {
-          inherit inputs;
-        };
+        inherit hosts users util;
       };
-    };
-  };
 }
--- util/default.nix
+++ util/default.nix
@@ -1,25 +1,53 @@
+# No-bullshit, simple utility function for creating dynamic, composable
+# multi-system configurations.
+
 { inputs, ... }:
 let
+  # Flake
   flake = ../flake.nix;
   pkgs = inputs.nixpkgs;
   home-manager = inputs.home-manager;
+
+  # Local Imports
+  deploy = import ./deploy.nix { inherit inputs; };
 in
-{
-  makeSystemConfiguration = { hardwareConfig, systemConfig, hmConfig ? {}, hmUsers, system ? "x86_64-linux", ... }@args:
+rec {
+  inherit deploy;
+
+  # Make one configuration
+  makeSystemConfiguration = { hardwareConfig, systemConfig, hmConfig ? {}, hmUsers ? {}, system ? "x86_64-linux", hostName ? "nixos", ... }@args:
     let
-      hmConfig =
+      hmModules =
         {
-          home-manager.useGlobalPkgs = true;
+          home-manager.useGlobalPkgs   = true;
           home-manager.useUserPackages = true;
-          home-manager.users = hmUsers;
+          home-manager.users           = hmUsers;
         } // hmConfig;
+
+      hostnameConfig =
+        {
+          networking.hostName = hostName;
+          # system.nixos.label = "";
+        };
     in
       pkgs.lib.nixosSystem {
         system = system;
         modules =
-          [ hardwareConfig
-            systemConfig
-            hmConfig
+          # System Configurations
+          hardwareConfig ++
+          systemConfig ++
+          [
+            hostnameConfig
+            # Home Manager
+            home-manager.nixosModules.home-manager hmModules
           ];
+
+        specialArgs = {
+          inherit inputs;
+        };
       };
+
+  # Make many configurations
+  makeSystemConfigurations = hosts:
+    builtins.mapAttrs (hostName: hostConfig: makeSystemConfiguration (hostConfig // { inherit hostName; })) hosts;
 }
