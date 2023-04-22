41bb38e0040f392aea43aee0de48304766300a53
m32 <m32@m32.io>
2023-04-22 15:58:39 -0700

    add support for backups that include data from podman containers

--- system/backup/default.nix
+++ system/backup/default.nix
@@ -1,6 +1,6 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 {
-  services.restic.backups = {
+  services.restic.backups = rec {
     rsyncnethome = {
       user = "m32";
       passwordFile = "/etc/nixos/secrets/r_pass";
@@ -23,6 +23,21 @@
         OnCalendar = "6h";
         RandomizedDelaySec = "1h";
       };
+    };
+
+    /** This backup instance is required if storing podman dev container data in the home directory
+     * the above configuration should be setup not to fail when the backup system can't access a file.
+     */
+    rsyncnethomepodman = rsyncnethome // {
+      package = pkgs.writeShellScriptBin "restic" ''
+              # Call restic within podman unsahre
+              exec ${pkgs.podman}/bin/podman unshare ${pkgs.restic}
+      '';
+      dynamicFilesFrom = "find /home/m32 -not -readable -a -type d";
+      paths = [];
+      extraOptions = [
+        "sftp.command='ssh fm1383@fm1383.rsync.net -i /home/m32/.ssh/id_rsa -o UserKnownHostsFile=/home/m32/.ssh/known_hosts -s sftp'"
+      ];
     };
   };
 }
