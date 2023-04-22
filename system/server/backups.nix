f8f11ca905c82d3a4bd0642dc5c6910b8bb56452
m32 <m32@m32.io>
2023-04-22 16:47:16 -0700

    refactor location of backup repo on remote

--- system/server/backups.nix
+++ system/server/backups.nix
@@ -7,12 +7,12 @@
       paths = [
         "/srv/"
       ];
-      repository = "sftp:fm1383@fm1383.rsync.net:backups/maple/srv/";
+      repository = "sftp:fm1383@fm1383.rsync.net:backups/srv/";
       extraOptions = [
         "sftp.command='ssh fm1383@fm1383.rsync.net -i /etc/nixos/secrets/id_backup -s sftp'"
       ];
       timerConfig = {
-        OnCalendar = "00:30";
+        OnCalendar = "12h";
         RandomizedDelaySec = "1h";
       };
     };
