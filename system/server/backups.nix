56d745c0a08a220faeeb7413b258646b5a5be90e
m32 <m32@m32.io>
2023-04-22 22:41:50 -0700

    Fix timer

--- system/server/backups.nix
+++ system/server/backups.nix
@@ -12,8 +12,8 @@
         "sftp.command='ssh fm1383@fm1383.rsync.net -i /etc/nixos/secrets/id_backup -s sftp'"
       ];
       timerConfig = {
-        OnActiveSec = "3h";
-        RandomizedDelaySec = "1h";
+        OnCalendar = "01,04,07,10,13,16,19,22:00:00";
+        RandomizedDelaySec = "15m";
       };
     };
   };
