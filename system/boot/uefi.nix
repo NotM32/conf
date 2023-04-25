cbaebb5230150aa56969c9c4d7e79fd9fc00899b
m32 <m32@m32.io>
2023-04-25 05:21:06 -0700

    Fix some boot issues...

--- system/boot/uefi.nix
+++ system/boot/uefi.nix
@@ -7,7 +7,7 @@
     device     = "nodev";
     efiSupport = true;
     configurationLimit = 5;
-    theme      = pkgs.breeze-grub;
+    # theme      = pkgs.breeze-grub;
 
     # won't work with just this FYI, there's a confirmation option for TPM
     # trustedBoot.enable = true;
