6d53b1666b9023645e89221f9a3e963f7e743d62
m32 <m32@m32.io>
2023-04-25 09:32:55 -0700

    add (temporarily universal) configuration for mixed dpi monitor layout...

--- system/X/kde.nix
+++ system/X/kde.nix
@@ -4,9 +4,20 @@
   services.xserver.enable = true;
 
   # SDDM Display Manager (kde)
-  services.xserver.displayManager.sddm.enable      = true;
-  services.xserver.displayManager.sddm.enableHidpi = true;
+  services.xserver.displayManager = rec {
+    sddm.enable = true;
+    sddm.enableHidpi = true;
+    sessionCommands = ''
+                    xrandr --output HDMI-0 --scale 2x2 --rate 240 --filter nearest \
+                    --output DP-2 --scale 1x1 --mode 4096x2160 --pos 2160x258 \
+                    --output DP-0 --scale 2x2 --rate 165 --pos 6256x582 --filter nearest
+    '';
+    setupCommands = sessionCommands;
+  };
+
 
   # Enable the Plasma 5 Desktop Environment.
   services.xserver.desktopManager.plasma5.enable   = true;
+
+
 }
