5fba840fd20d99176aab62630b49c0a28134b566
m32 <m32@m32.io>
2023-04-26 07:45:00 -0700

    remove things that belong somewhere else

--- system/X/displaylink.nix
+++ system/X/displaylink.nix
@@ -1,8 +1,6 @@
 { lib, pkgs, ... }:
 {
-
-  # X11 Video Drives
-  services.xserver.videoDrivers = [ "displaylink" "modesetting" "nvidia" ];
+  services.xserver.videoDrivers = [ "displaylink" "modesetting" ];
   hardware.opengl.enable        = true;
 
   # DisplayLink provider output sink enablement
@@ -10,9 +8,4 @@
       ${lib.getBin pkgs.xorg.xrandr}/bin/xrandr --setprovideroutputsource 2 0
   '';
 
-  services.xserver.displayManager.setupCommands    = ''
-    ${lib.getBin pkgs.xorg.xrandr}/bin/xrandr --setprovideroutputsource 2 0
-    ${lib.getBin pkgs.xorg.xrandr}/bin/xrandr --output DVI-D-0 --mode 1920x1080 --rate 239.96 --orientation left --right-of DVI-I-1-1
-    ${lib.getBin pkgs.xorg.xrandr}/bin/xrandr --output DVI-I-1-1 --mode 4096x2160 --rate 50
-  '';
 }
