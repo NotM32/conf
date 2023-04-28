206a5fa09f19c96d6852323fa9f170278a13f9ba
m32 <m32@m32.io>
2023-04-28 01:01:33 -0700

    wayland config...

---
+++ modules/system/storage.nix
@@ -0,0 +1,20 @@
+{ config, pkgs, lib, ... }:
+
+/* Opinionated module to define the disk layout of a system */
+
+with lib;
+
+let
+  cfg = config.system.storage;
+in {
+
+  imports = [ ];
+
+  options = {
+
+  };
+
+  config = {
+
+  };
+}
