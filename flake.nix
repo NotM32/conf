22624a06c65ec5cce4109e52416b41f38b2e7415
m32 <m32@m32.io>
2023-05-11 21:05:42 -0700

    refactor for consistency with how other flakes with libs did it

--- flake.nix
+++ flake.nix
@@ -1,41 +1,28 @@
 {
-  description = "m32.srv and system configurations";
+  description = "git.m32.me/conf/m32.conf - configuration for my systems";
 
   inputs = {
+    # Primary Package Repos
     nixpkgs.url = github:nixos/nixpkgs/nixpkgs-unstable;
     nur.url = github:nix-community/NUR;
-
     home-manager.url = github:nix-community/home-manager;
 
+    # Tools / Ops Utilities
     deploy.url = github:serokell/deploy-rs;
 
   };
 
-
   outputs = inputs@{ self, nixpkgs, home-manager, deploy, nur, ... }:
-    # Variables in the let scope are used for composing the actual user configurations,
-    # host configuration, hardware configurations, deploy nodes etc.
-
-    # They are meant to be the inputs provided to the utility functions that create the
-    # actual flake outputs.
-
-    # While the use of multiple let statements isn't as syntactically satisfying as the use
-    # of an attr set with a complex main function, keeping things scoped like this makes
-    # things a whole lot clearer in terms of what is actually happening.
     let
-      # - IMPORTS -
-      util = import ./util { inherit inputs; };
+      # - Local imports --
+      util = import ./lib { inherit inputs; };
 
-      # - BEGIN CONFIG -
+      # - System Configurations
 
-      # Users / Home Conf
+      # Users / Home Conf --
       users.m32 = import ./config/home.nix;
 
-    in
-    let
-      inherit users util;
-
-      # Host Composition
+      # Host Composition --
       hosts =
         { # ** Hosts
           # Desktop
@@ -89,18 +76,17 @@
             };
 
         };
-    in
-      # - END CONFIG -
-      {
-        # Configuration is done at this point, and this is the logic that doesn't change often
-        # used to build the actual flake outputs
+    in {
 
-        # * NixOS Configurations
-        nixosConfigurations = util.makeSystemConfigurations hosts;
+      # - NixOS Configurations
+      nixosConfigurations = util.makeSystemConfigurations hosts;
 
-        # * deploy-rs outputs
-        deploy.nodes = util.deploy.makeDeployNodes hosts self.nixosConfigurations;
+      # - deploy-rs outputs
+      deploy.nodes = util.deploy.makeDeployNodes hosts self.nixosConfigurations;
 
-        inherit hosts users util;
-      };
+      # - Lib outputs
+      lib = util;
+    };
+
+  # Nix COn
 }
