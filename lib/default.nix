2af90693a4dff04575f737809e96acea7d55e02d
m32 <m32@m32.io>
2023-05-20 06:05:32 -0700

    refactor and add flake-utils

--- lib/default.nix
+++ lib/default.nix
@@ -4,7 +4,6 @@
 { inputs, ... }:
 let
   # Flake
-  flake = ../flake.nix;
   pkgs = inputs.nixpkgs;
   home-manager = inputs.home-manager;
 
@@ -12,43 +11,30 @@ let
   deploy = import ./deploy.nix { inherit inputs; };
 in
 rec {
-  inherit deploy;
-
-  # Make one configuration
-  makeSystemConfiguration = { hardwareConfig,
-                              systemConfig,
-                              hmConfig ? { },
-                              users ? { },
-                              system ? "x86_64-linux",
-                              hostName ? "nixos",
-                              ... }@args:
+  /* Generate a system configuration supporting all the bits. */
+  makeSystemConfiguration = { hardwareProfile, systemConfig, hmConfig ? { }, users ? { }, hostName ? "nixos", ... }:
     let
+      # Home Mananger --
       hmModules =
         { home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
           home-manager.users = users;
         } // hmConfig;
 
+      # Set a default hostname --
       hostnameConfig = { networking.hostName = hostName; };
-    in
-    pkgs.lib.nixosSystem {
-      system = system;
-      modules =
-        # System Configurations
-        hardwareConfig ++
-        systemConfig ++
-        [ hostnameConfig
-          # Home Manager
-          home-manager.nixosModules.home-manager
-          hmModules
-        ];
-      # Give us access to flake inputs by providing it as an argument to the configuraiton module calls
+    in pkgs.lib.nixosSystem {
+      modules = systemConfig ++ [ hardwareProfile hostnameConfig home-manager.nixosModules.home-manager hmModules ];
+      # Give us access to flake inputs by providing it as an argument to the configuration module calls
       specialArgs = {
         inherit inputs;
       };
     };
 
-  # Make many configurations
+  /* Helper to perform makeSystemConfiguration on an attrset of multiple hosts */
   makeSystemConfigurations = hosts:
     builtins.mapAttrs (hostName: hostConfig: makeSystemConfiguration (hostConfig // { inherit hostName; })) hosts;
+
+  # Add other library modules here so they can be imported with 'import default'
+  inherit deploy;
 }
