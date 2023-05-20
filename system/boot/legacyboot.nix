8ffceedbbf9bb7ef33699efe2b9d6d2c5ad36c31
m32 <m32@m32.io>
2023-05-20 05:59:38 -0700

    remove deprecated grub version option

--- system/boot/legacyboot.nix
+++ system/boot/legacyboot.nix
@@ -2,7 +2,6 @@
 {
   # Use the GRUB 2 boot loader.
   boot.loader.grub.enable = true;
-  boot.loader.grub.version = 2;
   boot.loader.grub.efiSupport = true;
   # Remember to set the boot device in the hardware conf!
 }
