69454226d54298f03487a42d07ddda99dc0e06cd
m32 <m32@m32.io>
2023-06-03 02:29:14 -0700

    make this file pretty and satisfyingly symmetrical

--- lib/default.nix
+++ lib/default.nix
@@ -1,9 +1,9 @@
 inputs@{ ... }:
-let
-  # Local Imports
-  deploy = import ./deploy.nix { inherit inputs; };
-  system = import ./system.nix inputs;
-in
 {
-  inherit deploy system;
+  /* Utilities for deploy-rs */
+  deploy = import ./deploy.nix inputs;
+  /* Helpers for working with the `nixosConfigurations` flake output */
+  system = import ./system.nix inputs;
+  /* Extended library for use inside the NixOS configuration files */
+  config = import ./config.nix inputs;
 }
