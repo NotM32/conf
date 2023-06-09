628f1c52180bc973d9737cd1fa52f1770ce9cf89
m32 <m32@m32.io>
2023-06-08 18:46:47 -0700

    add secrets support

--- lib/default.nix
+++ lib/default.nix
@@ -2,8 +2,10 @@ inputs@{ ... }:
 {
   /* Utilities for deploy-rs */
   deploy = import ./deploy.nix inputs;
+
   /* Helpers for working with the `nixosConfigurations` flake output */
   system = import ./system.nix inputs;
+
   /* Extended library for use inside the NixOS configuration files */
   config = import ./config.nix inputs;
 }
--- lib/system.nix
+++ lib/system.nix
@@ -1,30 +1,40 @@
 /* Functions to streamline creating system compositions in the `nixosConfigurations`
    output of a flake
 */
-inputs@{ self, nixpkgs, home-manager, ... }:
-{
+inputs@{ self, nixpkgs, home-manager, sops-nix, ... }: {
   /* Generate a system configuration supporting all the bits. */
-  makeSystemConfiguration = { hardwareProfile, systemConfig, hmConfig ? { }, users ? { }, hostName ? "nixos", ... }:
+  makeSystemConfiguration = { hardwareProfile, systemConfig, hmConfig ? { }
+    , users ? { }, hostName ? "nixos", ... }:
     let
       # Home Mananger --
-      hmModules =
-        { home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.users = users;
-        } // hmConfig;
+      hmModules = {
+        home-manager.useGlobalPkgs = true;
+        home-manager.useUserPackages = true;
+        home-manager.users = users;
+      } // hmConfig;
 
       # Set a default hostname --
       hostnameConfig = { networking.hostName = hostName; };
 
     in nixpkgs.lib.nixosSystem {
-      modules = systemConfig ++ [ hardwareProfile hostnameConfig home-manager.nixosModules.home-manager hmModules ];
+      modules = systemConfig ++ [
+        hardwareProfile
+        hostnameConfig
+        # Home Manager --
+        home-manager.nixosModules.home-manager
+        hmModules
+        # Secrets --
+        sops-nix.nixosModules.sops
+      ];
       specialArgs = {
-        inherit inputs;    # Share access to the flake inputs in the configuration.
+        inherit inputs; # Share access to the flake inputs in the configuration.
         libm32 = self.lib; # Make this library available in the configuration.
       };
     };
 
   /* Helper to perform makeSystemConfiguration on an attrset of multiple hosts */
   makeSystemConfigurations = hosts:
-    builtins.mapAttrs (hostName: hostConfig: self.lib.system.makeSystemConfiguration (hostConfig // { inherit hostName; })) hosts;
+    builtins.mapAttrs (hostName: hostConfig:
+      self.lib.system.makeSystemConfiguration
+      (hostConfig // { inherit hostName; })) hosts;
 }
