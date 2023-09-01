f11cc633409110cae347beeb6f2fc293ad831073
m32 <m32@m32.io>
2023-09-01 15:03:32 -0700

    actually nevermind on these

--- config/system/default.nix
+++ config/system/default.nix
@@ -1,12 +1,9 @@
-inputs@{ ... }:
+{ ... }:
 let
   layersPath = ./layers;
   rolesPath = ./roles;
 in {
   conf.system = {
     inherit layersPath rolesPath;
-
-    roles = import rolesPath;
-    layers = import layersPath inputs;
   };
 }
--- config/system/layers/default.nix
+++
@@ -1,8 +0,0 @@
-{ nixpkgs, ... }:
-let
-  lib = nixpkgs.lib;
-  categories = builtins.attrNames
-    (lib.filterAttrs (k: v: v == "directory") builtins.readDir ./.);
-in {
-
-}
