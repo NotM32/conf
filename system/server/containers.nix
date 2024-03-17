3d559eae1dd5c2880c4f4b84d15718e38a03833c
m32 <m32@m32.io>
2024-03-16 23:11:07 -0700

    container support

---
+++ system/server/containers.nix
@@ -0,0 +1,9 @@
+{ pkgs, ... }:
+{
+  virtualisation.podman.enable = true;
+  virtualisation.podman.dockerSocket.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    docker-compose
+  ];
+}
