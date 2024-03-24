7a857cd358d468eb3d7e0995e684bcc89e8b89f3
m32 <m32@m32.io>
2024-03-24 14:14:43 -0700

    update configurationrevision options to use correct flake input

--- system/default.nix
+++ system/default.nix
@@ -1,4 +1,4 @@
-{ pkgs, inputs, lib, config, ... }:
+{ pkgs, lib, config, self, ... }:
 
 {
   time.timeZone = "America/Los_Angeles";
@@ -104,17 +104,17 @@
 
   # Store commit data in generation label
   system.nixos.label =
-    lib.mkIf (inputs.self ? rev)
-    "${inputs.self.shortRev}-${lib.concatStringsSep "-" ((lib.sort (x: y: x < y) config.system.nixos.tags))}";
+    lib.mkIf (self ? rev)
+    "${self.shortRev}-${lib.concatStringsSep "-" ((lib.sort (x: y: x < y) config.system.nixos.tags))}";
 
   # Also store commit revision where it can be accessed with `nixos-version`
-  system.configurationRevision = inputs.self.rev or "dirty";
+  system.configurationRevision = self.rev or "dirty";
 
   # And finally, link the latest version of the subscribed flake to /etc/nixos, so it can be built from.
   # this is a pure flake option similar to that of system.copySystemConfiguration, but for another purpose
   environment.etc = {
     "nixos" = {
-      source = "${inputs.self.sourceInfo.outPath}/**";
+      source = "${self.sourceInfo.outPath}/**";
     };
   };
 
