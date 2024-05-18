4d88084e99efeae227a73cbe24ef54a6c6737c9a
m32 <m32@m32.io>
2024-05-17 22:38:36 -0600

    tiling: add hyprland and refactor kde support

--- modules/desktop/kde.nix
+++ modules/desktop/kde.nix
@@ -1,31 +1,13 @@
 { pkgs, ... }: {
-  hardware.opengl.enable = true;
 
-  services.xserver.enable = true;
-  programs.xwayland.enable = true;
-
-  # SDDM Display Manager (kde)
-  services.xserver.displayManager = {
-    sddm.enable = true;
-    sddm.enableHidpi = true;
-    defaultSession = "plasma";
-  };
+  imports = [ ./default.nix ./sddm.nix ];
 
   # Enable the Plasma 6 Desktop Environment.
   services.desktopManager.plasma6.enable = true;
 
+  services.xserver.displayManager.defaultSession = "plasma";
+
   environment.plasma6.excludePackages = with pkgs.libsForQt5; [ konsole elisa ];
 
-  environment.systemPackages = with pkgs; [
-    kleopatra
-
-    # Utils
-    glxinfo
-    clinfo
-    wayland-utils
-    vulkan-tools
-  ];
-
-  # Some Electron apps show black screens in wayland unless this is set
-  environment.sessionVariables.NIXOS_OZONE_WL = "1";
+  environment.systemPackages = with pkgs; [ kleopatra ];
 }
