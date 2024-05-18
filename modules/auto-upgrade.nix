fbc48454c16d7f26312970640cf6387a2317bbc4
m32 <m32@m32.io>
2024-05-17 21:42:30 -0600

    auto-upgrade: disable auto-upgrades when built from dirty tree

--- modules/auto-upgrade.nix
+++ modules/auto-upgrade.nix
@@ -1,11 +1,10 @@
-{ ... }:
-{
+{ self, lib, ... }: {
   # Automatic Upgrades / Configuration sync
-  system.autoUpgrade = {
+  system.autoUpgrade = lib.mkIf (self ? rev) {
     enable = true;
     flake = "m32conf";
     flags = [
-      "--impure" #TODO: remove the things that make this flake evaluate impurely
+      "--impure" # TODO: remove the things that make this flake evaluate impurely
     ];
   };
 }
