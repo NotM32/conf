d44ace810a2c71ca53878f86fe17ab8ca41654eb
m32 <m32@m32.io>
2024-05-22 12:24:05 -0600

    dolphin and qt

--- modules/desktop/hyprland.nix
+++ modules/desktop/hyprland.nix
@@ -1,6 +1,11 @@
-{ ... }:
+{ pkgs, ... }:
 {
   imports = [ ./sddm.nix ];
+
+  environment.systemPackages = with pkgs; [
+    qt6.qtwayland
+    qt5.qtwayland
+  ];
 
   programs.hyprland = {
     enable = true;
