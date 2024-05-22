aeb3715cba8dcfe1b3c8624fbe717939be7da8ff
m32 <m32@m32.io>
2024-05-22 03:16:33 -0600

    wallpaperd and wofi modules

---
+++ home/hyprland/wpaperd.nix
@@ -0,0 +1,11 @@
+{ ... }: {
+  programs.wpaperd.enable = true;
+
+  programs.wpaperd.settings = {
+    default = {
+      duration = "30m";
+      mode = "center";
+      path = "/home/m32/media/pictures/wallpapers/orange";
+    };
+  };
+}
