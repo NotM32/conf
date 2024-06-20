427d31e5e4e4c641a583c23060cbfa3757d0cfee
m32 <m32@m32.io>
2024-06-20 13:32:16 -0600

    create isolated emacs module

---
+++ home/emacs/default.nix
@@ -0,0 +1,53 @@
+{ pkgs, spacemacs, lib, ... }:
+let
+
+in {
+  imports = [ ../mail ];
+
+  home.packages = with pkgs; [
+    # ## Language Servers
+    tflint # terraform linter
+    terraform-ls # terraform language server
+    helm-ls # helm (kubernetes package manager) language server
+    gopls # gopls
+    texlab # texlab
+    hadolint
+    dockerfile-language-server-nodejs
+    perlPackages.PerlLanguageServer
+    clippy
+    rustfmt
+    cargo-edit
+    cargo-outdated
+    rubyPackages.solargraph
+    rubyPackages.pry
+    rubyPackages.pry-doc
+    rubyPackages.ruby_parser
+    rubyPackages.rubocop
+    rubyPackages.prettier
+
+    # Virtual Term support lib
+    libvterm
+  ];
+
+  programs.emacs = {
+    enable = true;
+    # from emacs-overlay, resolved screen flicker and scaling issues
+    package = pkgs.emacs-pgtk;
+  };
+
+  # spacemacs installation
+  home.file.".spacemacs".source = ./emacs/spacemacs.el;
+  home.file.".emacs.d" = {
+    source = spacemacs;
+    recursive = true;
+  };
+
+  services.emacs = {
+    enable = true;
+    defaultEditor = true;
+    startWithUserSession = true;
+  };
+
+  mail.mbsync.enable = true;
+
+}
