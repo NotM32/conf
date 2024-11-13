59d834942197eb344708e2e3873e34eb0b560c42
m32 <m32@m32.io>
2024-11-13 08:13:01 -0700

    set display options

--- hosts/nova.nix
+++ hosts/nova.nix
@@ -26,14 +26,20 @@
   home-manager.users.m32 = {
     imports = [ self.homeModules.desktop-tiling ];
 
+
     wayland.windowManager.hyprland.settings = {
+      env = [ "GDK_SCALE,1" ];
+
       monitor = [
+        "desc:California Institute of Technology 0x161D 0x00006001, 2560x1600@165.02Hz, auto, 1.6"
         "desc:Sceptre Tech Inc E24 0x01010101, 1920x1080x74.97, 0x0, 1"
         "desc:Dell Inc. DELL S2721HS CXFN7P3, 1920x1080@74.97, 1920x0, 1"
         "desc:Lenovo Group Limited Y25-25 U4HDVK7C, 1920x1080@239.96, 3840x0, 1"
         "desc:Ancor Communications Inc MX279 G8LMRS025036,disable"
         "Unknown-1,disable"
       ];
+
+      xwayland.force_zero_scaling = true;
     };
   };
 
