47503e17a4ae86d2afe06321f1117a0bc1455a4b
m32 <m32@m32.io>
2024-11-24 15:40:34 -0700

    change domain name

--- modules/networking/zerotier.nix
+++ modules/networking/zerotier.nix
@@ -2,10 +2,7 @@
   services.zerotierone = {
     enable = true;
     joinNetworks = [
-      # Churchill Interlink
       "35c192ce9b255366"
-
-      # M32 Srv Interlink
       "233ccaac27077fe3"
     ];
   };
