752c00b55fe4abfbc8135a684da85c2ab6b7786e
m32 <m32@m32.io>
2024-11-26 22:08:20 -0700

    move nix configuration to nix module

--- modules/nix.nix
+++ modules/nix.nix
@@ -1,6 +1,4 @@
-{ pkgs, ... }: {
-  # Use the latest version of nix
-
+{ pkgs, nur, home-manager, flake-registry, nixpkgs, ... }: {
   nix.settings = {
     system-features = [ "recursive-nix" "kvm" "nixos-test" "big-parallel" ];
     experimental-features = [ "nix-command" "flakes" "recursive-nix" ];
@@ -16,5 +14,21 @@
   nix.optimise = {
     automatic = true;
     dates = [ "weekly" ];
+  };
+
+  nix.nixPath = [
+    "nixpkgs=${pkgs.path}"
+    "nur=${nur}"
+    "home-manager=${home-manager}"
+  ];
+
+  nix.extraOptions = ''
+        flake-registry = ${flake-registry}/flake-registry.json
+                     '';
+
+  nix.registry = {
+    home-manager.flake = home-manager;
+    nixpkgs.flake = nixpkgs;
+    nur.flake = nur;
   };
 }
