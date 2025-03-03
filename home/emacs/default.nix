1333d9a2527992a609da775f5f8bec2486ed2dcd
m32 <m32@m32.io>
2025-03-03 01:27:18 -0700

    add tex packages for emacs org mode

--- home/emacs/default.nix
+++ home/emacs/default.nix
@@ -1,20 +1,31 @@
 { config, pkgs, ... }:
 let
-  emacs = with pkgs; (emacsPackagesFor emacs-pgtk).emacsWithPackages (epkgs: with epkgs; [
-    treesit-grammars.with-all-grammars
-    vterm
-    notmuch
-    mbsync
-    offlineimap
-  ]);
+  emacs = with pkgs;
+    (emacsPackagesFor emacs-pgtk).emacsWithPackages (epkgs:
+      with epkgs; [
+        treesit-grammars.with-all-grammars
+        vterm
+        notmuch
+        mbsync
+        offlineimap
+      ]);
 
-  doomReloadScript = with pkgs; writeScript "doom-reload"
-    ''
-    #!/bin/sh
-    export PATH="${emacs}/bin:$PATH"
+  doomReloadScript = with pkgs;
+    writeScript "doom-reload" ''
+      #!/bin/sh
+      export PATH="${emacs}/bin:$PATH"
 
-    ${config.home.homeDirectory}/.emacs.d/bin/doom -y sync -u
+      ${config.home.homeDirectory}/.emacs.d/bin/doom -y sync -u
     '';
+
+  tex = (pkgs.texlive.combine {
+    inherit (pkgs.texlive)
+      scheme-basic dvisvgm dvipng # for preview and export as html
+      wrapfig amsmath ulem hyperref capt-of
+      etoolbox metafont;
+    #(setq org-latex-compiler "lualatex")
+    #(setq org-preview-latex-default-process 'dvisvgm)
+  });
 in {
   home.packages = with pkgs; [
     black
@@ -53,33 +64,31 @@ in {
     stylelint
     svelte-language-server
     terraform-ls
+    tex
     texlab
-    texlive.combined.scheme-medium
     tflint
     typescript-language-server
     yaml-language-server
     zig
   ];
 
-  home = {
-    sessionPath = [ "${config.home.homeDirectory}/.emacs.d/bin" ];
- };
+  home = { sessionPath = [ "${config.home.homeDirectory}/.emacs.d/bin" ]; };
 
- programs.emacs = {
+  programs.emacs = {
     enable = true;
     package = emacs;
   };
 
-   # doom-emacs configuration
-   # home.file.".doom.d" = {
-   #   source = ./doom/.;
-   #   recursive = true;
-   #   onChange = "${doomReloadScript}";
-   # };
+  # doom-emacs configuration
+  # home.file.".doom.d" = {
+  #   source = ./doom/.;
+  #   recursive = true;
+  #   onChange = "${doomReloadScript}";
+  # };
 
-   services.emacs = {
-     enable = true;
-     defaultEditor = true;
-     startWithUserSession = true;
-   };
+  services.emacs = {
+    enable = true;
+    defaultEditor = true;
+    startWithUserSession = true;
+  };
 }
