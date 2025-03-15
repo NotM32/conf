2b12acbe7fbc14be7b608de54f76e46f4a8e3df6
m32 <m32@m32.io>
2025-03-15 04:44:36 -0600

    update firejail profiles

--- modules/security/firejail.nix
+++ modules/security/firejail.nix
@@ -6,9 +6,15 @@ in {
     enable = true;
 
     wrappedBinaries = {
+      signal-desktop = {
+        executable = "${lib.getBin pkgs.signal-desktop}/bin/signal-desktop";
+        profile = "${profiles}/signal-desktop.profile";
+        desktop = "${pkgs.signal-desktop}/share/applications/signal-desktop.desktop";
+      };
       spotify = {
         executable = "${lib.getBin pkgs.spotify}/bin/spotify";
         profile = "${profiles}/spotify.profile";
+        desktop = "${pkgs.spotify}/share/applications/spotify.desktop";
       };
     };
   };
