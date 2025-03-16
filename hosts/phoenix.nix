cedce0f3f6c63cb406e56c3dd0c75b108e38b67c
m32 <m32@m32.io>
2025-03-16 03:28:02 -0600

    dont turn off displays

--- hosts/phoenix.nix
+++ hosts/phoenix.nix
@@ -44,10 +44,6 @@
         { timeout = 240;
           on-timeout = "loginctl lock-session";
         }
-        { timeout = 300;
-          on-timeout = "hyprctl dispatch dpms off";
-          on-resume = "hyprctl dispatch dpms on";
-        }
       ];
     };
   };
