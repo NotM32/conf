02a9b2a0c20ca79524076374d7b9fa4e756d33a5
m32 <m32@m32.io>
2025-04-18 08:20:21 -0600

    add cuda packages to unfree predicate

--- modules/services/ollama.nix
+++ modules/services/ollama.nix
@@ -1,6 +1,10 @@
 { config, lib, pkgs, ... }:
 
 {
+  allowUnfreePackages = [
+    "cuda-.*"
+  ];
+
   services.ollama = {
     enable = true;
     acceleration = if config.hardware.nvidia.open then false else "cuda";
