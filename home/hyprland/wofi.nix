15f83e5bde6da4d8e6bf8c62030e52a0aa9bcfe0
m32 <m32@m32.io>
2025-04-24 01:41:26 -0600

    create wofi theme

--- home/hyprland/wofi.nix
+++ home/hyprland/wofi.nix
@@ -1,5 +1,38 @@
-{ ... }: {
+{ pkgs, ... }: {
   programs.wofi = {
     enable = true;
+
+    settings = { gtk_dark = true; };
+
+    style = ''
+      * {
+        font-family: 'Hack Nerd Font';
+        font-size: 12px;
+      }
+
+      #window,
+      #input,
+      #inner-box,
+      #expander-box {
+        color: #E4E4E8;
+        background-color: #181818;
+        border: none;
+        outline: none;
+      }
+
+      #entry {
+        background-color: #181818;
+      }
+
+      #entry:selected {
+        background-color: #91B9C7;
+      }
+
+      #input:active {
+        border-color: #91B9C7;
+      }
+    '';
   };
+
+  home.packages = with pkgs; [ wofi-power-menu ];
 }
