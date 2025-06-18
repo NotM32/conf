da6971251ef3473f489f2dae7037ea0d4c0d6a61
m32 <m32@m32.io>
2025-06-18 00:26:05 -0600

    regreet now

--- modules/desktop/display-manager.nix
+++ modules/desktop/display-manager.nix
@@ -1,9 +1,44 @@
-{ pkgs, ... }: {
+{ pkgs, self, ... }:
+let
+  hyprlandConfig = pkgs.writeText "config" ''
+    exec-once = ${pkgs.greetd.regreet}/bin/regreet; hyprctl dispatch exit
+    misc {
+         disable_hyprland_logo = true
+         disable_splash_rendering = true
+         disable_hyprland_qtutils_check = true
+    }
+  '';
+in {
+  programs.regreet = {
+    enable = true;
+
+    theme = {
+      package = pkgs.kanagawa-gtk-theme;
+      name = "Kanagawa-BL-LB";
+    };
+
+    iconTheme = {
+      package = pkgs.kanagawa-icon-theme;
+      name = "Kanagawa";
+    };
+
+    font = {
+      package = pkgs.nerd-fonts.hack;
+      name = "Hack Nerd Font";
+    };
+
+    settings = {
+      background.path = "${self}/home/desktop/wallpaper.png";
+      background.fit = "Cover";
+    };
+  };
+
   services.greetd = {
     enable = true;
+
     settings = {
       default_session = {
-        command = "${pkgs.greetd.tuigreet}/bin/tuigreet";
+        command = "${pkgs.hyprland}/bin/Hyprland --config ${hyprlandConfig}";
         user = "greeter";
       };
     };
