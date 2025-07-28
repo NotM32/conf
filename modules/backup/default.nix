f63529bc8f19402923373d421272352567517263
m32 <m32@m32.io>
2025-07-28 01:18:02 -0600

    tag backups with the shortrev of the flake used to build the system

--- modules/backup/default.nix
+++ modules/backup/default.nix
@@ -7,10 +7,13 @@ let
     initialize = false;
 
     extraOptions = [
-      ''sftp.command="/bin/sh -c 'ssh $(cat ${config.sops.secrets."backup_repo/connection".path}) -i ${config.sops.secrets."backup_repo/ssh_id".path} -o UserKnownHostsFile=${config.sops.secrets."backup_repo/known_hosts".path} -s sftp'" ''
+      ''sftp.command="/bin/sh -c 'ssh $(cat ${config.sops.secrets."backup_repo/connection".path}) -i ${
+        config.sops.secrets."backup_repo/ssh_id".path
+      } -o UserKnownHostsFile=${config.sops.secrets."backup_repo/known_hosts".path} -s sftp'" ''
     ];
   };
-in {
+in
+{
   options = {
     backups = {
       home.enable = mkOption {
@@ -42,7 +45,11 @@ in {
       srv = repoConfig // {
         paths = [ "/srv/" ];
 
-        extraBackupArgs = [ "--option read-concurrency=10" "--tag automatic" ];
+        extraBackupArgs = [
+          "--option read-concurrency=10"
+          "--tag automatic"
+          "--tag ${self.shortRev or "dirtyflake"}"
+        ];
 
         timerConfig = {
           OnCalendar = "01,04,07,10,13,16,19,22:00:00";
@@ -51,42 +58,59 @@ in {
       };
 
       # * General backup of home directory hosts.
-      home = mkIf (config.backups.home.enable) (repoConfig // {
-        paths = [
-          "/home/m32/media"
-          "/home/m32/docs"
-          "/home/m32/downloads"
-          "/home/m32/projects"
-        ];
-        exclude = [ "/home/m32/games/*" ];
+      home = mkIf (config.backups.home.enable) (
+        repoConfig
+        // {
+          paths = [
+            "/home/m32/media"
+            "/home/m32/docs"
+            "/home/m32/downloads"
+            "/home/m32/projects"
+          ];
+          exclude = [ "/home/m32/games/*" ];
 
-        extraBackupArgs = [ "--option read-concurrency=10" "--tag automatic" "--tag home" ];
+          extraBackupArgs = [
+            "--option read-concurrency=10"
+            "--tag automatic"
+            "--tag home"
+            "--tag ${self.shortRev or "dirtyflake"}"
+          ];
 
-        timerConfig = {
-          OnCalendar = "hourly";
-          Persistent = true;
-          RandomizedDelaySec = "15m";
-        };
+          timerConfig = {
+            OnCalendar = "hourly";
+            Persistent = true;
+            RandomizedDelaySec = "15m";
+          };
 
-        createWrapper = false;
-      });
+          createWrapper = false;
+        }
+      );
 
-      homeFull = mkIf (config.backups.home.enable) (repoConfig // {
-        paths = [
-          "/home/m32"
-        ];
-        exclude = [ "/home/m32/games/*" ];
+      homeFull = mkIf (config.backups.home.enable) (
+        repoConfig
+        // {
+          paths = [
+            "/home/m32"
+          ];
+          exclude = [ "/home/m32/games/*" ];
 
-        extraBackupArgs = [ "--option read-concurrency=10" "--tag automatic" "--tag home" "--tag full" ];
+          extraBackupArgs = [
+            "--option read-concurrency=10"
+            "--tag automatic"
+            "--tag home"
+            "--tag full"
+          "--tag ${self.shortRev or "dirtyflake"}"
+          ];
 
-        timerConfig = {
-          OnCalendar = "00,12:00:00";
-          Persistent = true;
-          RandomizedDelaySec = "15m";
-        };
+          timerConfig = {
+            OnCalendar = "00,12:00:00";
+            Persistent = true;
+            RandomizedDelaySec = "15m";
+          };
 
-        createWrapper = false;
-      });
+          createWrapper = false;
+        }
+      );
     };
   };
 }
