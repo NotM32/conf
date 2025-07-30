8aab8b48acef650ffd76299507d2601f03782637
m32 <m32@m32.io>
2025-07-30 00:35:30 -0600

    remove wofi style

--- home/hyprland/wofi.nix
+++ home/hyprland/wofi.nix
@@ -1,37 +1,11 @@
-{ pkgs, ... }: {
+{ pkgs, ... }:
+{
   programs.wofi = {
     enable = true;
 
-    settings = { gtk_dark = true; };
-
-    style = ''
-      * {
-        font-family: 'Hack Nerd Font';
-        font-size: 12px;
-      }
-
-      #window,
-      #input,
-      #inner-box,
-      #expander-box {
-        color: #E4E4E8;
-        background-color: #181818;
-        border: none;
-        outline: none;
-      }
-
-      #entry {
-        background-color: #181818;
-      }
-
-      #entry:selected {
-        background-color: #91B9C7;
-      }
-
-      #input:active {
-        border-color: #91B9C7;
-      }
-    '';
+    settings = {
+      gtk_dark = true;
+    };
   };
 
   home.packages = with pkgs; [ wofi-power-menu ];
