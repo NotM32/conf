52460adf3fb2d37da62c8e923d1a213b3e8b6205
m32 <m32@m32.io>
2025-08-31 11:05:03 -0600

    repo: cleanup

--- devShells/flake-module.nix
+++ devShells/flake-module.nix
@@ -16,7 +16,6 @@
           pkgs.ipmitool
           inputs.nixos-anywhere.packages.${system}.nixos-anywhere-pxe
           pkgs.pixiecore
-          pkgs.dnsmasq
           pkgs.openssh
           pkgs.gitMinimal
           pkgs.rsync
@@ -27,19 +26,23 @@
           pkgs.gnused
           pkgs.sops
           pkgs.jq
+          pkgs.nushell
+
+          # terraform
           pkgs.opentofu
 
+          # pam tools
           pkgs.pam_u2f
           pkgs.otpw
           pkgs.oath-toolkit
           pkgs.pamtester
 
+          # secureboot
           pkgs.sbctl
         ] ++ pkgs.lib.optional (pkgs.stdenv.isLinux) pkgs.mkpasswd;
 
         shellHook = ''
-            alias devdocs='mdbook serve --port 3025 --open ./docs/'
-            alias mkdocs='nix build .#docs'
+            # nu -e 'use scripts/'
           "
         '';
       };
